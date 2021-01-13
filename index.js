d304740a122252d35324533b917340aebf6eaf33
John <john.bow7720@gmail.com>
2021-01-13 16:01:41 -0700

    3 working selenium tests

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
 
+const driver = new Builder().forBrowser('firefox').build();
+
+async function navToFcc() {
+  try {
+    await driver.get('http://google.com');
+    await driver.findElement(By.name('q')).sendKeys('johnbowser.dev', Key.ENTER);
+    await driver.wait(until.elementLocated(By.id('search')));
+    await driver.findElement(By.partialLinkText('John Bowser - Full Stack Developer')).click()
+    const title = await driver.getTitle();
+
+    console.log(title);
+  } catch(e) { console.log(e)}
+  // finally {
+  //   await driver.quit();
+  // }
+
+}
+
+navToFcc();
--- test/google-test.spec.js
+++ test/google-test.spec.js
@@ -1,6 +1,6 @@
 const { expect } = require("chai");
 const { done } = require('mocha');
-const { Builder } = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 
 describe('test test', () => {
   it('1+1=2', () => {
@@ -13,13 +13,41 @@ describe('Selenium google title test', () => {
   const driver = new Builder().forBrowser('firefox').build();
 
   it('should navigate to Google and find the page title', async () => {
-    await (await driver).get('http://google.com');
+    await driver.get('http://google.com');
     const title = await driver.getTitle();
-    console.log(title);
     expect(title).to.eql('Google');
   });
 
-  after(async () => {
-    await driver.quit()
-  });
+  after(async () => await driver.quit());
 });
+
+describe('Selenium google search input', () => {
+  const driver = new Builder().forBrowser('firefox').build();
+
+  it('should navigate to google and insert a search term', async () => {
+    await driver.get('http://google.com');
+    const search = await driver.findElement(By.name('q'))
+    await search.sendKeys('Hello');
+    const searchText = await search.getAttribute('value');
+
+    expect(searchText).to.eql('Hello');
+  });
+
+  after(async () => driver.quit());
+});
+
+describe('find my personal website', () => {
+  const driver = new Builder().forBrowser('firefox').build();
+
+  it('should find my portfolio and get the title', async () => {
+    await driver.get('http://google.com');
+    await driver.findElement(By.name('q')).sendKeys('johnbowser.dev', Key.ENTER);
+    await driver.wait(until.elementLocated(By.id('search')));
+    await driver.findElement(By.partialLinkText('John Bowser - Full Stack Developer')).click();
+    const title = await driver.getTitle();
+
+    expect(title).to.eql('John Bowser - Full Stack Developer');
+  });
+
+  after(async () => driver.quit());
+})
